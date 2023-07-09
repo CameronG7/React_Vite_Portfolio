388d4e642035d0d83586b169f7c9f01a88e712aa
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-07-09 14:58:21 -0700

    resume embed mobile next

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const handleDownload = () => {
 	console.log('clicked handleDownload');
 };
 
-const handleView = () => {	
+const handleView = () => {
 	console.log('clicked handleView');
 };
 
-
 export const Resume = () => {
+	const isDesktop = useMediaQuery({ minWidth: 1024 });
+
 	return (
-		<div className="prose mx-auto my-2 ">
-			<h1 className=" justify-center">Resume </h1>
-			<p>blah blah resume blah blah</p>
+		<div decide>
+			{isDesktop ? (
+				<div desktop>
+					<div className="prose mx-auto my-2 ">
+						<h1 className=" justify-center">Resume </h1>
+						<p>blah blah resume blah blah</p>
+					</div>
+					<iframe
+						className="w-full md:w-1/2 min-h-screen  md:mx-auto mb-2"
+						src="https://docs.google.com/document/d/e/2PACX-1vT16VJMyQET9S-PNRopSgsniAWHWNm-5sZ-ic8e7-Brff8jx9HVuAnbI6xiyWZnjA/pub?embedded=true"
+					></iframe>
+				</div>
+			) : (
+				<div mobile>
+					
+				</div>
+			)}
 		</div>
 	);
 };
