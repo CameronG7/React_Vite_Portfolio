d151e7fc77f17e1486292c58d2ce7e5a81c9b23d
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-07-12 21:34:04 -0700

    image reorg

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -6,7 +6,7 @@ export const Portfolio = () => {
 		<div className="h-96 carousel carousel-vertical rounded-box min-h-screen">
 			{/* reel */}
 			<Project
-				imageUrl="src/assets/Portfolio/reel.png"
+				imageUrl="src/assets/reel.png"
 				hasDeployedApp 
 				githubRepoUrl="https://www.github.com/CameronG7/ReelReviews"
 				deployedAppUrl="https://sheltered-reef-22263.herokuapp.com/"
@@ -14,35 +14,35 @@ export const Portfolio = () => {
 			/>
 			{/* jate */}
 			<Project
-				imageUrl="src/assets/Portfolio/JATE.png"
+				imageUrl="src/assets/JATE.png"
 				hasDeployedApp 
 				githubRepoUrl="https://www.github.com/CameronG7/JATE_PWA"
 				deployedAppUrl="https://salty-temple-36941-0e4bb5ef543d.herokuapp.com/"
 				projectName="JATE PWA"
 			/>
 			<Project
-				imageUrl="src/assets/Portfolio/emp_db.png"
+				imageUrl="src/assets/emp_db.png"
 				hasDeployedApp = {false} 
 				githubRepoUrl="https://www.github.com/CameronG7/employeeData_mySQL"
 				deployedAppUrl="https://www.github.com/CameronG7"
 				projectName="MYSQL Employee Tracker"
 			/>
 			<Project
-				imageUrl="src/assets/Portfolio/weather.png"
+				imageUrl="src/assets/weather.png"
 				hasDeployedApp 
 				githubRepoUrl="https://www.github.com/CameronG7/5_day_weather"
 				deployedAppUrl="https://camerong7.github.io/5_day_weather/"
 				projectName="3rd Party API Weather Dashboard"
 			/>
 			<Project
-				imageUrl="src/assets/Portfolio/social api.png"
+				imageUrl="src/assets/social api.png"
 				hasDeployedApp = {false}
 				githubRepoUrl="https://github.com/CameronG7/social_media_api"
 				deployedAppUrl="https://www.github.com/CameronG7"
 				projectName="MongoDB Social Network API"
 			/>
 			<Project
-				imageUrl="src/assets/Portfolio/backend.jpg"
+				imageUrl="src/assets/backend.jpg"
 				hasDeployedApp = {false}
 				githubRepoUrl="https://www.github.com/CameronG7/ORM_eCommerce_Backend"
 				deployedAppUrl="https://www.github.com/CameronG7"
